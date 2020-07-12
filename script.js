dfd7cac298bdd898a60aec57de52b07e408c3257
Jacinto Wong <37252418+JacintoDesign@users.noreply.github.com>
2020-07-12 13:50:47 -0400

    final production code

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const gamePage = document.getElementById('game-page');
 const scorePage = document.getElementById('score-page');
 const splashPage = document.getElementById('splash-page');
 const countdownPage = document.getElementById('countdown-page');
-// Countdown Page
-const countdown = document.querySelector('.countdown');
 // Splash Page
 const startForm = document.getElementById('start-form');
 const radioContainers = document.querySelectorAll('.radio-container');
 const radioInputs = document.querySelectorAll('input');
-// Best Score Page
 const bestScores = document.querySelectorAll('.best-score-value');
+// Countdown Page
+const countdown = document.querySelector('.countdown');
+// Game Page
 const itemContainer = document.querySelector('.item-container');
-// Final Score Page
+// Score Page
 const finalTimeEl = document.querySelector('.final-time');
 const baseTimeEl = document.querySelector('.base-time');
 const penaltyTimeEl = document.querySelector('.penalty-time');
